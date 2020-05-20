0f647f9395356897368dad98e3a95152ee917736
borys <borys.smejda@gmail.com>
2020-05-20 12:52:03 +0200

    Starting writing main function

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "WindowHandler.hpp"
+
+int main()
+{
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    #ifdef __APPLE__
+        std::cout << "We are on MacOS\n";
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    return 0;
+}
